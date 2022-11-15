ca7dfa3a849e36303cd6adb9c00f42fc92584492
AlexandraCherepania <106696097+AlexandraCherepania@users.noreply.github.com>
2022-11-15 18:15:54 +0200

    update

---
+++ js/02-lightbox.js
@@ -0,0 +1,42 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+console.log(galleryItems);
+const lightboxRef = document.querySelector(".gallery");
+const lightboxMarkup = createBoxItem(galleryItems);
+lightboxRef.insertAdjacentHTML(`afterbegin`, lightboxMarkup);
+
+
+function createBoxItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div >
+`;
+    })
+      .join('');
+};
+
+lightboxRef.addEventListener("click", onBoxRefClick);
+
+instance.show();
+function onBoxRefClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
